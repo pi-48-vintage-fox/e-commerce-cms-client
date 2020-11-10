5bfe1aca908de6e5363fbea6a2c8c1f2fe3acb72
musabagja <bagja7musa@gmail.com>
2020-11-10 23:35:54 +0700

    Vue Structure Learned

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <LandingPage v-if="!loggedIn" @toLogin="toLogin"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingPage from '../components/LandingPage'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      loggedIn: false
+    }
+  },
+  methods: {
+    toLogin (status) {
+      this.loggedIn = status
+    }
+  },
+  components: {
+    LandingPage
+  },
+  created () {
+    if (localStorage.getItem('token')) {
+      this.$router.push('/about')
+    }
+  }
+}
+</script>

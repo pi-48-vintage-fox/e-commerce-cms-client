bb05fca6b427c69e3ad8f82e6aedaf3d021ab632
Haris Pratama <hpratama324@gmail.com>
2020-11-11 01:42:46 +0700

    Haris Pratama - e-Commerce CMS Client

---
+++ src/components/ProductsPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-3">
+    <div class="card">
+      <img class="card-img-top" :src="product.image_url" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"><a href="">{{ product.name }}</a></h5>
+        <p class="card-text">
+          <span class="badge badge-primary">{{ product.Category.name }}</span>
+        </p>
+      </div>
+      <div class="card-footer">
+        <p class="card-text">{{ product.price }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductPage',
+  props: ['product']
+}
+</script>
+
+<style>
+  img {
+    height: 200px;
+  }
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <h1>E-Commerce CMS</h1>
+    <div class="container">
+      <div class="row">
+        <ProductsPage
+          v-for="product in products" :key="product.id"
+          :product="product"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from '../axios/axiosinstance.js'
+import ProductsPage from '@/components/ProductsPage.vue'
+
+export default {
+  name: 'Product',
+  data () {
+    return {
+      products: []
+    }
+  },
+  components: {
+    ProductsPage
+  },
+  methods: {
+    fetchProducts () {
+      axios
+        .get('/products')
+        .then(({ data }) => {
+          this.products = data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  created () {
+    this.fetchProducts()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

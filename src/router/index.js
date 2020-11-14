e98a734c38e12d6027742233fd68beec1dd2475a
ReynaldiSeptian19 <reynaldiseptian019@gmail.com>
2020-11-14 17:16:20 +0700

    done

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/LoginForm.vue'
+import Product from '../views/Product'
+import addProduct from '../views/AddProduct'
+import editProduct from '../views/EditProduct'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/Product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/addProduct',
+    name: 'addProduct',
+    component: addProduct
+  },
+    {
+    path: '/editProduct/:id',
+    name: 'editProduct',
+    component: editProduct
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login' && localStorage.access_token) next({ path: '/Product' })
+  else if (to.path === '/Product' && !localStorage.access_token) next({ name: 'Login' })
+  else next()
+})
+
+export default router
---
+++ src/views/EditProduct.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container" id="editProduct">
+        <form @submit.prevent="editProduct" >
+          <div style="width: 50%; margin: 200px auto;">
+            <h1>Edit</h1>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" v-model="dataEdit.name" class="form-control" id="edit-name"><br/>
+              <label for="image_url">image url</label>
+              <input type="text" v-model="dataEdit.image_url" class="form-control" id="edit-image_url"><br/>
+              <label for="price">Price</label>
+              <input type="text" v-model="dataEdit.price" class="form-control" id="edit-price"><br/>
+              <label for="stock">Stock</label>
+              <input type="text" v-model="dataEdit.stock" class="form-control" id="edit-stock"><br/>
+            </div>
+            <div class="row">
+              <div class="col">
+                <button type="submit" class="btn btn-primary form-control">Edit</button>
+              </div>
+          </div>
+          </div>
+        </form>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'editProduct',
+  computed: {
+      dataEdit(){
+          return this.$store.state.dataEdit
+      }
+  },
+  methods: {
+      editProduct(){
+          const payload = {
+              id:this.$route.params.id,
+              name: this.dataEdit.name,
+              image_url: this.dataEdit.image_url,
+              price: this.dataEdit.price,
+              stock: this.dataEdit.stock
+          }
+          console.log(payload)
+          this.$store.dispatch('editProduct' ,payload)
+            .then(({data})=>{
+                this.$router.push({path: '/Product'})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
